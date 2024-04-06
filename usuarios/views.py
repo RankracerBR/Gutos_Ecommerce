79536287a0bc800e640db8514941f8763630f4c2
RankracerBR <augustopontes010@gmail.com>
2024-04-06 12:30:47 -0300

    Alterações nas views

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def acesso_conta(request):
-    ...
+def pagina_usuarios(request):
+    return render(request, 'usuarios.html')
+
+def pagina_conta(request):
+    return render(request, 'pagina_conta.html')
 
 def acesso_extrato(request):
     ...
