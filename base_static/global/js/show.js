4358b6802bbdd3a4e7614b1ab0be59e7728902ac
RankracerBR <augustopontes010@gmail.com>
2024-04-06 12:26:13 -0300

    Alteração no base static e no base templates

---
+++ base_static/global/js/show.js
@@ -0,0 +1,16 @@
+function toggleUrls(moduleId) {
+    var urls = document.getElementById(moduleId);
+    if (urls.style.display === 'none') {
+        urls.style.display = 'block';
+    } else {
+        urls.style.display = 'none';
+    }
+}
+
+document.getElementById('acesso').addEventListener('click', function() {
+    toggleUrls('urls-acesso');
+});
+
+document.getElementById('usuarios').addEventListener('click', function() {
+    toggleUrls('urls-usuarios');
+});
