574e8e32d22f7aa5d7ef109853a6d0b9980dd3dc
RankracerBR <augustopontes010@gmail.com>
2024-04-09 11:35:30 -0300

    Alterações nas rotas

--- acesso/urls.py
+++ acesso/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from acesso import views
 
+app_name = "acesso"
+
 urlpatterns = [
     path('', views.pagina, name="pagina"),
+    path('verificacao/', views.verificacao_token, name="verificacao"),
+    path('assinatura/', views.assinatura, name="assinatura"),
+    path('verificacao/<str:token>/', views.verificacao, name="verificar"),
+    path('cadastro/', views.cadastro, name="cadastro"),
+    path('comentarios/', views.comentarios, name="comentarios"),
+    path('logout', views.funcao_logout, name="logout"),
+    path('login/', views.pagina_login, name="login")
 ]
