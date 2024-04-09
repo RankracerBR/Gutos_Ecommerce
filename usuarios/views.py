ca61f444fa93935db85b29f9595e3eb2822f5a50
RankracerBR <augustopontes010@gmail.com>
2024-04-09 11:43:02 -0300

    Alterações na views e autênticação

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404 , get_list_or_404
+from django.urls import reverse_lazy
+from .models import Usuario
+
 
 # Create your views here.
 
 def pagina_usuarios(request):
     return render(request, 'usuarios.html')
 
+@login_required(login_url='acesso:login')
 def pagina_conta(request):
     return render(request, 'pagina_conta.html')
 
