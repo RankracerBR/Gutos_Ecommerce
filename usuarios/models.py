f3597ab8116fba3a1f9a412bb9eb56a0584af451
RankracerBR <augustopontes010@gmail.com>
2024-04-09 11:42:15 -0300

    Alteração do modelo de usuários

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,21 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
-class Usuario(models.Model):
-    nome = models.CharField(max_length=40, default='nome_completo')
-    idade = models.PositiveSmallIntegerField()
-    email = models.EmailField()
-    senha = models.TextField(max_length=150, verbose_name="Senha")
+class Usuario(AbstractUser):
+    idade = models.PositiveSmallIntegerField(default=0)
     imagem = models.ImageField(upload_to='imgs/')
-
-    def __str__(self):
-        return self.nome
-
-class Produto(models.Model):
-    nome_produto = models.CharField(max_length=100, default=None)
-    valor_produto = models.FloatField()
-    tipo_produto = models.CharField(max_length=50, default=None)
-
-    def __str__(self):
-        return self.nome_produto
