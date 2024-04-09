3fcf902586ab1f46e7234a99ff9bc55937be3574
RankracerBR <augustopontes010@gmail.com>
2024-04-09 11:41:55 -0300

    Alterações no painel do admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,9 +1,24 @@
-from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
+from django.contrib import admin
+from acesso.forms import UsuarioRegistroForm
+from django.conf import settings
+from django.contrib import messages
+import psycopg2
+
+# Register your models here.
 
 @admin.register(Usuario)
-class AdminUsuario(admin.ModelAdmin):
-    list_display = ('nome', 'email','imagem')
-    list_filter = ('nome', 'email', 'imagem')
+class UsuarioAdmin(UserAdmin):
+    add_form = UsuarioRegistroForm
+    list_display = ('username', 'email', 'imagem')
+    list_filter =  ('username', 'email', 'imagem')
 
-
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 
+                                             'imagem')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 
+                                   'groups', 'user_permissions')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
+    )
